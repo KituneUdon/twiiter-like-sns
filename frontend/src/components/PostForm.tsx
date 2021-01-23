32bea598768bf374b25443a26e787784af791d5b
KituneUdon <kituneudon97@gmail.com>
2021-01-23 20:05:23 +0900

    #7 画像を追加するためのボタンを追加

--- frontend/src/components/PostForm.tsx
+++ frontend/src/components/PostForm.tsx
@@ -1,19 +1,29 @@
 import React, { FC } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Col, Row, Form } from 'react-bootstrap';
 
 import styled from 'styled-components';
 
-const TextArea = styled.textarea`
-  display: block;
+const WideButton = styled(Button)`
+  width: 100%;
+  margin-bottom: 1vh;
 `;
 
 const PostForm: FC = () => (
   <Container>
-    <form>
-      <TextArea />
-      <Button variant="primary">投稿</Button>
-    </form>
+    <Row>
+      <Col>
+        <Form>
+          <Form.Group>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <WideButton variant="primary">投稿</WideButton>
+          <Form.Group>
+            <Form.File id="exampleFormControlFile1" />
+          </Form.Group>
+        </Form>
+      </Col>
+    </Row>
   </Container>
-)
+);
 
 export default PostForm;
