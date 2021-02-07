2bf101f5f7e05f46b7f595741b20e9106553c116
KituneUdon <kituneudon97@gmail.com>
2021-02-07 11:08:15 +0900

    #13 APIから値を取得するための処理のひな型をコンポーネントに追加した

--- frontend/src/apis/getMicroposts.ts
+++ frontend/src/apis/getMicroposts.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const getTimeline = async () => {
+  // URLをサーバサイドのAPIにする
   const response = await axios.get('http://localhost:3000/').then((res) => res);
 
   return response;
--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 import TimelineElement from './TimelineElement';
 
-const Timeline: FC = () => (
-  <>
-    <p>タイムライン</p>
-    <hr />
-    <TimelineElement name="kituneudon" />
-  </>
-);
+import getMicroposts from '../apis/getMicroposts';
+
+const Timeline: FC = () => {
+  const [microposts, setMicroposts] = useState();
+
+  // APIからデータを取得してstateに格納する処理を記載する
+  useEffect(() => {
+    const response = getMicroposts();
+
+    response.then((m) => Array.isArray(m) && setMicroposts(m));
+  }, []);
+
+  return (
+    <>
+      <p>タイムライン</p>
+      <hr />
+      <TimelineElement name="kituneudon" />
+    </>
+  );
+};
 
 export default Timeline;
