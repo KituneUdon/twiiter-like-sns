fd4b39a8645d0558a0ad8af16b603374b638f9bf
KituneUdon <kituneudon97@gmail.com>
2021-04-08 01:56:45 +0000

    #20 取得したプロフィールを表示するためのコードを作成

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
+import { AxiosResponse } from 'axios';
 
 import profileIcon from '../image/default_profile_icon.png';
 import UserInfo from './UserInfo';
+import getProfile from '../apis/getProfile'
 
 const Img = styled.img`
   width: 100%;
@@ -11,22 +13,51 @@ const Img = styled.img`
   height: auto;
 `;
 
-const Profile: FC = () => (
-  <Container>
-    <Row>
-      <Col md={4} sm={12} xs={4}>
-        <Img src={profileIcon} alt="プロフィール画像" />
-      </Col>
-      <Col md={8} sm={12} xs={8}>
-        <p>kituneudon</p>
-        <a href="http://localhost:4000">view my profile</a>
-        <p>投稿数：10</p>
-      </Col>
-    </Row>
-    <Row>
-      <UserInfo />
-    </Row>
-  </Container>
-);
+type userprofileType = {
+  displayName: string;
+  follower: number;
+  following: number;
+  postedNumber: number;
+}
+
+const Profile: FC = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [follower, setFollower] = useState(0);
+  const [following, setFollowing] = useState(0);
+  const [postedNumber, setPostedNumner] = useState(0);
+
+  useEffect(() => {
+    const response = getProfile('123456');
+
+    response.then((data: AxiosResponse<userprofileType>) => {
+      setDisplayName(data.data.displayName);
+      setFollower(data.data.follower);
+      setFollowing(data.data.following);
+      setPostedNumner(data.data.postedNumber);
+    }).catch(() => {
+      setErrorMessage('通信エラーが発生しました。')
+    })
+  })
+
+  return (
+    <Container>
+      {errorMessage && <p>{errorMessage}</p>}
+      <Row>
+        <Col md={4} sm={12} xs={4}>
+          <Img src={profileIcon} alt="プロフィール画像" />
+        </Col>
+        <Col md={8} sm={12} xs={8}>
+          <p>{displayName}</p>
+          <a href="http://localhost:4000">view my profile</a>
+          <p>投稿数：{postedNumber}</p>
+        </Col>
+      </Row>
+      <Row>
+        <UserInfo follower={follower} following={following} />
+      </Row>
+    </Container>
+  );
+}
 
 export default Profile;
--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -2,15 +2,20 @@ import React, { FC } from 'react';
 
 import { Container, Row, Col } from 'react-bootstrap';
 
-const UserInfo: FC = () => (
+type Props = {
+  follower: number;
+  following: number;
+}
+
+const UserInfo: FC<Props> = ({follower, following}) => (
   <Container>
     <Row>
       <Col>
-        <p>10</p>
+        <p>{follower}</p>
         <p>follower</p>
       </Col>
       <Col>
-        <p>20</p>
+        <p>{following}</p>
         <p>following</p>
       </Col>
     </Row>
