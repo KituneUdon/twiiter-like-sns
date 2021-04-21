6f16d207c9209cdabed6b24c5443105f0ed5fd24
KituneUdon <kituneudon97@gmail.com>
2021-04-21 02:18:45 +0000

    #14 投稿機能を作成した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './App.css';
 
 import { Container, Col, Row } from 'react-bootstrap';
@@ -8,21 +8,26 @@ import PostForm from './components/PostForm';
 import Microposts from './components/Micropost';
 import Header from './components/Header';
 
-const App: FC = () => (
-  <>
-    <Header />
-    <Container>
-      <Row>
-        <Col xs={12} sm={4}>
-          <Profile />
-          <PostForm />
-        </Col>
-        <Col xs={12} sm={8}>
-          <Microposts />
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+const App: FC = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  return (
+    <>
+      <Header />
+      {errorMessage && <p>{errorMessage}</p>}
+      <Container>
+        <Row>
+          <Col xs={12} sm={4}>
+            <Profile />
+            <PostForm id='123456' setErrorMessage={setErrorMessage}/>
+          </Col>
+          <Col xs={12} sm={8}>
+            <Microposts />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default App;
--- frontend/src/components/PostForm.tsx
+++ frontend/src/components/PostForm.tsx
@@ -1,29 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Button, Container, Col, Row, Form } from 'react-bootstrap';
 
 import styled from 'styled-components';
+
+import postMicropost from '../apis/postMicropost'
 
 const WideButton = styled(Button)`
   width: 100%;
   margin-bottom: 1vh;
 `;
 
-const PostForm: FC = () => (
-  <Container>
-    <Row>
-      <Col>
-        <Form>
-          <Form.Group>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-          <WideButton variant="primary">投稿</WideButton>
-          <Form.Group>
-            <Form.File id="exampleFormControlFile1" />
-          </Form.Group>
-        </Form>
-      </Col>
-    </Row>
-  </Container>
-);
+type Props = {
+  id: string;
+  setErrorMessage: (errorMessage: string) => void;
+}
+
+const PostForm: FC<Props> = ({id, setErrorMessage}) => {
+  const [formText, setFormText] = useState('');
+
+  const handlePostMicropost = () => {
+    postMicropost(id, formText).catch(() => setErrorMessage('通信エラーが発生しました。'))
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group>
+              <Form.Control as="textarea" rows={3} onChange={(e) => setFormText(e.target.value)} />
+            </Form.Group>
+            <WideButton variant="primary" onClick={() => handlePostMicropost()}>投稿</WideButton>
+            <Form.Group>
+              <Form.File id="exampleFormControlFile1" />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default PostForm;
