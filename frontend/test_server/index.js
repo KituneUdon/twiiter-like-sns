c5c3708ef6834bca2ca7d3e85412b84b15814560
KituneUdon <kituneudon97@gmail.com>
2021-04-30 10:19:00 +0900

    20 create profile function (#22)
    * #20 APIを叩いてデータを取得するコードを作成
    
    * #20 取得したプロフィールを表示するためのコードを作成
    
    * #20 テストのバックエンドサーバを用意
    
    * #20 OpenAPIで決めたAPIの形式に合わせた
    
    * #20 変更漏れがあったので修正
    
    * #20 プロフィール情報を管理するstateを一つに集約
    
    * #20 ユーザプロフィールの型定義を別ファイルにした

--- frontend/test_server/index.js
+++ frontend/test_server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 
 const microposts = require('./jsons/microposts.json');
+const userprofile = require('./jsons/userprofile.json');
 
 const app = express();
 app.use(cors())
@@ -9,6 +10,14 @@ app.use(cors())
 app.get('/v1/microposts', (req, res) => {
   if (req.query.id === '123456') {
     res.json(microposts);
+  } else {
+    res.send(400);
+  }
+});
+
+app.get('/v1/userprofile', (req, res) => {
+  if (req.query.id === '123456') {
+    res.json(userprofile);
   } else {
     res.send(400);
   }
